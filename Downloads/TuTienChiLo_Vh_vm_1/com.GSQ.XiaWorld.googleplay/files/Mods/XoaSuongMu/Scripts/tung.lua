e158e947401cc98493b816e02ab08e6384d23ed2
unknown <duongdinhdat6@gmail.com>
2026-05-01 20:46:32 +0700

    add file

---
+++ Downloads/TuTienChiLo_Vh_vm_1/com.GSQ.XiaWorld.googleplay/files/Mods/XoaSuongMu/Scripts/tung.lua
@@ -0,0 +1,18 @@
+﻿local TungMOD = GameMain:GetMod("tung");
+local time = 0;
+local flag = 0;
+
+if Gameplay then
+    Gameplay.GameSpeed = 10.0
+end
+
+function TungMOD:OnStep(dt)
+    if flag == 0 then
+        time = time + dt;
+        if time >= 5 then
+            flag = 1;
+            CS.GameMain.Instance.NoFog = true;
+            CS.MapRender.Instance.Fog.enabled = false;
+        end
+    end
+end
